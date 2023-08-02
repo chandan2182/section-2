431b20d1173a646a824272162eaca5112ade1f62
chandan2182 <139637586+chandan2182@users.noreply.github.com>
2023-08-02 20:08:03 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div>
+     <h1>My React App</h1>
+     <Home />
+     <Login />
+     <Signup />
+    </div>
+  );
+}
+
+export default App;
